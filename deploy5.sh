c3482ee57a3df0935197ed36194d6344c40cd979
“ragutransappdata” <s.raghunath@transappdata.com>
2018-11-05 00:24:17 -0500

    this is fifth commit

---
+++ deploy5.sh
@@ -0,0 +1,12 @@
+. /u03/env/ORCL.env
+which sqlplus
+sqlplus system/passwd  <<EOF
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+select sysdate+1 from dual;
+SELECT username FROM dba_users WHERE username='RAGHU';
+DROP table RAGHU.name;
+EOF
+export VAR1=`echo $?`
+export VAR1=`echo $?`
+exit $VAR1
